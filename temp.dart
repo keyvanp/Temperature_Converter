17990f15e1d0baf69b644effa38b7df8d777c9a5
Keyvan <keyvan.perez2@gmail.com>
2015-11-22 04:47:41 +0000

    Made better use of the try and simplified the code.

--- temp.dart
+++ temp.dart
@@ -2,25 +2,20 @@ import 'dart:io';
 
 void main(List<String> arguments) {
     String tempType = "";
-    double temp = 0.0;
-    bool noError = false;
     stdout.write("Enter the type of temperature (C or F): ");
     tempType = stdin.readLineSync();
     if (tempType == "c" || tempType == "C" || tempType == "f" || tempType == "F") {
         stdout.write("Enter the temperature: ");
         try {
             // Read from the console and convert it to double
-            temp = double.parse(stdin.readLineSync());
-            noError = true;
-        } on FormatException catch (e) {
-            stdout.writeln("The entered input (${e.source}), is not a temperature.");
-        }
-        if (noError) {
+            double temp = double.parse(stdin.readLineSync());
             if (tempType == "c" || tempType == "C") {
                 stdout.writeln("The temperature in Fahrenheit is: ${temp * 1.8 + 32.0}");
             } else if (tempType == "f" || tempType == "F") {
                 stdout.writeln("The temperature in Celsius is: ${(temp - 32.0) / 1.8}");
             }
+        } on FormatException catch (e) {
+            stdout.writeln("The entered input (${e.source}), is not a temperature.");
         }
     } else {
         stdout.writeln("The entered input is not an option.");
